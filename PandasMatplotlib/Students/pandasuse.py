098f4bd196010f7d3a8e20c7d3de33dbe25a6a1f
AmericanEagle1776 <bonnlass@hu-berlin.de>
2018-11-23 15:00:33 +0100

    lol

--- PandasMatplotlib/Students/pandasuse.py
+++ PandasMatplotlib/Students/pandasuse.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-whole = pd.read_csv('data.csv')
+import pandas as pd
 
+df = pd.read_csv('data.csv', sep=';', header = 0, decimal=',', na_values ='-', index_col = 0)
 
+c = df.mean()
+d = df.mode()
+a = df.groupby('Identifier').mean()
+b = df['A'].where(df['Classifier'] == 1.0).value_counts()
